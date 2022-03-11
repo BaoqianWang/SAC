aa85c19cbc5343f0a7d625f3285d7cfd2a7e248a
BaoqianWang <you@example.com>
2022-03-11 15:48:37 -0800

    1

--- amazon_scripts/run_wmc.sh
+++ amazon_scripts/run_wmc.sh
@@ -25,9 +25,10 @@ python3 ../experiments/train_darl1n.py \
     --scenario=wireless_mc \
     --num-agents=9 \
     --num-learners=9 \
-    --save-dir="../result/wmc/darl1n/9agents/" \
+    --save-dir="../result/wmc/darl1n/" \
     --save-rate=10 \
     --max-num-train=400 \
     --max-num-neighbors=9 \
     --eva-max-episode-len=25 \
+    --ddl=10 \
     --seed=16 \
